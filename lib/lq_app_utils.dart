3988706de1e89938e72e45ee4ada2bf9f072cb2e
lq <>
2023-06-19 22:04:12 +0800

    移除第三方

--- lib/lq_app_utils.dart
+++ lib/lq_app_utils.dart
@@ -11,14 +11,10 @@ export 'extensions/string_extension.dart';
 
 //utils
 export 'utils/color_utils.dart';
-export 'utils/device_utils.dart';
 export 'utils/json_utils.dart';
 export 'utils/object_utils.dart';
-export 'utils/package_utils.dart';
 export 'utils/regex_utils.dart';
-export 'utils/screen_info_utils.dart';
 export 'utils/system_utils.dart';
-export 'utils/utils_init.dart';
 export 'utils/app_error_interceptor.dart';
 export 'utils/log_utils.dart';
 
@@ -27,19 +23,9 @@ export 'mixin/state_app_lifecycle_mixin.dart';
 export 'mixin/state_scroll_mixin.dart';
 export 'mixin/state_context_ready_mixin.dart';
 
-//network
-export 'network/network_status_manager.dart';
-export 'network/network_type.dart';
-
-//storage
-export 'storage/sp_utils.dart';
-
 //widgets
 export 'widgets/after_layout_view.dart';
 export 'widgets/bottom_nav_bar.dart';
 export 'widgets/keyboard_dismiss.dart';
 export 'widgets/layout_log_view.dart';
 export 'widgets/scroll_notification_listener.dart';
-
-//第三方
-export 'package:device_info_plus/device_info_plus.dart';
